379c45c3fcc1fe08a0c480aa0f3730d737c2ed83
Johora-Juhi <zohrajuhi1012@gmail.com>
2024-05-24 22:46:20 +0600

    centalized routes

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
-import { StudentRoutes } from "./app/modules/student/student.route";
-import { UserRoutes } from "./app/modules/user/user.routes";
 import globalErrorHandler from "./app/middleware/globalErrorHandler";
 import notFound from "./app/middleware/notFound";
+import router from "./app/routes";
+
 const app: Application = express();
 
 // parser
@@ -11,8 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 // application route
-app.use("/api/v1/student", StudentRoutes);
-app.use("/api/v1/users", UserRoutes);
+app.use("/api/v1", router);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("hello world");
@@ -23,4 +22,5 @@ app.use(notFound);
 
 // error handler middleware
 app.use(globalErrorHandler);
+
 export default app;
