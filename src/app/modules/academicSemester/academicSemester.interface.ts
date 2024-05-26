154787e7c8048b3e566d1818b65dff4c197cb0eb
Johora-Juhi <zohrajuhi1012@gmail.com>
2024-05-27 00:11:36 +0600

    academic semester model added

--- src/app/modules/academicSemester/academicSemester.interface.ts
+++ src/app/modules/academicSemester/academicSemester.interface.ts
@@ -1,4 +1,4 @@
-type Month =
+export type TMonths =
   | "January"
   | "February"
   | "March"
@@ -12,10 +12,13 @@ type Month =
   | "November"
   | "December";
 
+export type TAcademicSemesterName = "Autumn" | "Summer" | "Fall";
+export type TAcademicSemesterCode = "01" | "02" | "03";
+
 export type TAcademicSemester = {
-  name: "Autumn" | "Summer" | "Fall";
-  code: "01" | "02" | "03";
+  name: TAcademicSemesterName;
+  code: TAcademicSemesterCode;
   year: Date;
-  startMonth: Month;
-  endMonth: Month;
+  startMonth: TMonths;
+  endMonth: TMonths;
 };
