ada204ab74cad4c4360656067b9814fec539713c
Johora-Juhi <zohrajuhi1012@gmail.com>
2024-05-29 23:25:23 +0600

    added query transaction

--- src/app/modules/user/user.services.ts
+++ src/app/modules/user/user.services.ts
@@ -7,6 +7,7 @@ import { Student } from "../student/student.model";
 import { TUser } from "./user.interface";
 import { User } from "./user.model";
 import { generateStudentId } from "./user.utils";
+import mongoose from "mongoose";
 
 const createStudentIntoDB = async (password: string, studentData: TStudent) => {
   // if (await Student.isStudentExists(studentData.id)) {
@@ -28,20 +29,43 @@ const createStudentIntoDB = async (password: string, studentData: TStudent) => {
     throw new AppError(httpStatus.NOT_FOUND, "Academic semester not found");
   }
 
-  // set student id
-  userData.id = await generateStudentId(academicSemesterData);
+  // start the transaction session
+  const session = await mongoose.startSession();
 
-  // create user
-  const newUser = await User.create(userData); //builtin static method
-
-  if (Object.keys(newUser).length) {
+  try {
+    session.startTransaction();
     // set student id
-    studentData.id = newUser.id;
-    studentData.user = newUser._id;
+    userData.id = await generateStudentId(academicSemesterData);
 
-    const newStudent = await Student.create(studentData);
+    // create user
+    const newUser = await User.create([userData], { session }); //builtin static method
 
-    return newStudent;
+    if (!newUser.length) {
+      throw new AppError(httpStatus.BAD_REQUEST, "Failed to create new user");
+    }
+
+    if (newUser.length) {
+      // set student id
+      studentData.id = newUser[0].id;
+      studentData.user = newUser[0]._id;
+
+      const newStudent = await Student.create([studentData], { session });
+
+      if (!newStudent.length) {
+        throw new AppError(
+          httpStatus.BAD_REQUEST,
+          "Failed to create new student"
+        );
+      }
+
+      session.commitTransaction();
+      session.endSession();
+
+      return newStudent;
+    }
+  } catch (error) {
+    session.abortTransaction();
+    session.endSession();
   }
 
   // for creatin instance
