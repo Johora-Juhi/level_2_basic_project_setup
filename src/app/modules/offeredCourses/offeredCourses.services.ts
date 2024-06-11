38a5905b71f5cbcf5db591946716ae5a918195b2
Johora-Juhi <zohrajuhi1012@gmail.com>
2024-06-11 22:12:05 +0600

    offered courses validation done

--- src/app/modules/offeredCourses/offeredCourses.services.ts
+++ src/app/modules/offeredCourses/offeredCourses.services.ts
@@ -14,6 +14,7 @@ const createOfferedCoursesIntoDB = async (payload: TOfferedCourses) => {
     academicFaculty,
     academicDepartment,
     course,
+    section,
     faculty,
   } = payload;
 
@@ -53,6 +54,32 @@ const createOfferedCoursesIntoDB = async (payload: TOfferedCourses) => {
 
   if (!isFacultyExists) {
     throw new AppError(httpStatus.NOT_FOUND, "Faculty is not found!");
+  }
+
+  const isFacultyExistsInDepartment = await AcademicDepartment.findOne({
+    _id: academicDepartment,
+    academicFaculty,
+  });
+
+  if (!isFacultyExistsInDepartment) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      `This ${isAcademicFacultyExists.name} does not belong to ${isAcademicDepartmentExists.name}`
+    );
+  }
+
+  const isSameSectionWithSameCourseWithSameSemesterExists =
+    await OfferedCourse.findOne({
+      semesterRegistration,
+      course,
+      section,
+    });
+
+  if (isSameSectionWithSameCourseWithSameSemesterExists) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      `This course with same section already exists!`
+    );
   }
 
   const result = await OfferedCourse.create({ ...payload, academicSemester });
