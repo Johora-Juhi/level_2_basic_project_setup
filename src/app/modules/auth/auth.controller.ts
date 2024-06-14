56f50adc91c34165fd7c6b926450311eddfba003
Johora-Juhi <zohrajuhi1012@gmail.com>
2024-06-14 19:41:30 +0600

    set refresh token in cookie

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -2,9 +2,16 @@ import httpStatus from "http-status";
 import catchAsync from "../../middleware/catchAsync";
 import SendResponse from "../../utils/sendResponse";
 import { AuthServices } from "./auth.services";
+import config from "../../config";
 
 const loginUser = catchAsync(async (req, res) => {
   const result = await AuthServices.loginUser(req.body);
+  const { refreshToken } = result;
+
+  res.cookie("refreshToken", refreshToken, {
+    secure: config.node_env === "production",
+    httpOnly: true,
+  });
 
   SendResponse(res, {
     statusCode: httpStatus.OK,
